c4f23ed2988f2183d3ad0873b22fb6fab501805a
Partiolainen <petersson.kiev@gmail.com>
2018-08-29 22:59:02 +0300

    demo app and minor changes

---
+++ CoinlibApi.Demo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoinlibApi.Types;
+
+namespace CoinlibApi.Demo
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var apikey = ""; //Place your api key here
+            var client = new CoinlibClient(apikey);
+
+            var globalinfo = await client.Global("BTC");
+            Console.WriteLine($"{DateTime.Now:G}\nGlobal market cap: {globalinfo.TotalMarketCap} BTC");
+            Console.WriteLine();
+
+            var worstcoin = (await client.Coinlist(1, "BTC", CoinlistOrder.rank_desc)).Coins.FirstOrDefault();
+            Console.WriteLine($"{DateTime.Now:G}\n" +
+                              $"Worst rank coin: {worstcoin?.Name} with rank #{worstcoin?.Rank}");
+            Console.WriteLine();
+
+            var zen = await client.Coin("ZEN", "BTC");
+            Console.WriteLine($"{DateTime.Now:G}\nZEN price: {zen.Price} BTC");
+            Console.WriteLine();
+
+            var coinslist = new List<string>() {"XEM", "XRP", "TRX"};
+            var coins = (await client.Coins(coinslist, "BTC")).Coins;
+            var bestoflist = coins.OrderBy(x => x.Rank).FirstOrDefault();
+            Console.WriteLine($"{DateTime.Now:G}\nBest rank coin of {string.Join(", ", coinslist)}:" +
+                              $" {bestoflist?.Symbol} with rank #{bestoflist?.Rank}");
+
+            Console.ReadKey();
+        }
+    }
+}
--- CoinlibApi/CoinlibClient.cs
+++ CoinlibApi/CoinlibClient.cs
@@ -10,39 +10,55 @@ namespace CoinlibApi
 	{
 		public CoinlibClient(string apikey)
 		{
-			ApiKey = apikey;
+			this.apikey = apikey;
 		}
 
-		private readonly string ApiKey;
+		private readonly string apikey;
 
 		private const string baseurl = "https://coinlib.io/api/v1";
 
 		public async Task<GlobalResponse> Global(string pref = "USD")
 		{
-			var http = new HttpClient();
-			var response = await http.GetStringAsync($"{baseurl}/global?key={ApiKey}&pref={pref}");
-			return JsonConvert.DeserializeObject<GlobalResponse>(response);
+		    string response;
+		    using (var http = new HttpClient())
+		    {
+		        response = await http.GetStringAsync($"{baseurl}/global?key={apikey}&pref={pref}");
+		    }
+
+		    return JsonConvert.DeserializeObject<GlobalResponse>(response);
 		}
 
 		public async Task<CoinlistResponse> Coinlist(int page = 1, string pref = "USD", CoinlistOrder order = CoinlistOrder.rank_asc)
 		{
-			var http = new HttpClient();
-			var response = await http.GetStringAsync($"{baseurl}/coinlist?key={ApiKey}&pref={pref}&page={page}&order={order.ToString()}");
-			return JsonConvert.DeserializeObject<CoinlistResponse>(response);
+		    string response;
+		    using (var http = new HttpClient())
+		    {
+		        response = await http.GetStringAsync($"{baseurl}/coinlist?key={apikey}&pref={pref}&page={page}&order={order.ToString()}");
+		    }
+
+		    return JsonConvert.DeserializeObject<CoinlistResponse>(response);
 		}
 
 		public async Task<CoinsResponseCoin> Coin(string symbol, string pref = "USD")
 		{
-			var http = new HttpClient();
-			var response = await http.GetStringAsync($"{baseurl}/coin?key={ApiKey}&pref={pref}&symbol={symbol}");
-			return JsonConvert.DeserializeObject<CoinsResponseCoin>(response);
+		    string response;
+		    using (var http = new HttpClient())
+		    {
+		        response = await http.GetStringAsync($"{baseurl}/coin?key={apikey}&pref={pref}&symbol={symbol}");
+		    }
+
+		    return JsonConvert.DeserializeObject<CoinsResponseCoin>(response);
 		}
 
 		public async Task<CoinsResponse> Coins(List<string> symbols, string pref = "USD")
 		{
-			var http = new HttpClient();
-			var response = await http.GetStringAsync($"{baseurl}/coin?key={ApiKey}&pref={pref}&symbol={string.Join(",", symbols)}");
-			return JsonConvert.DeserializeObject<CoinsResponse>(response);
+		    string response;
+		    using (var http = new HttpClient())
+		    {
+		        response = await http.GetStringAsync($"{baseurl}/coin?key={apikey}&pref={pref}&symbol={string.Join(",", symbols)}");
+		    }
+
+		    return JsonConvert.DeserializeObject<CoinsResponse>(response);
 		}
 	}
 }
